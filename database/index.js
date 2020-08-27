eabca10dc8e2cfcc5fe755a5e91dd990da116c5e
Joseph Dry <joe.dry6477@gmail.com>
2020-08-27 11:04:37 -0400

    Added put route

--- database/index.js
+++ database/index.js
@@ -48,7 +48,6 @@ const addRestaurant = ({restaurant, articles, features}, cb) => {
   values = values.substring(1, values.length - 1);
   connection.query(`INSERT INTO restaurants (${columns}) VALUES (${values});`, (err, response) => {
     if (err) {
-      console.log(err)
       cb(err);
     } else {
       const restaurantId = response.insertId;
@@ -60,7 +59,7 @@ const addRestaurant = ({restaurant, articles, features}, cb) => {
         connection.query(`INSERT INTO articles (restaurant_id, ${columns}) VALUES (${restaurantId}, ${values})`, (err) => {
           if (err) {
             cb(err);
-          } else {
+          } else if (i === articles.length - 1) {
             // Add each feature with appropriate restaurant id
             columns = Object.keys(features[0]).join(', ');
             features.forEach((feature, i) => {
@@ -69,8 +68,8 @@ const addRestaurant = ({restaurant, articles, features}, cb) => {
               connection.query(`INSERT INTO features (restaurant_id, ${columns}) VALUES (${restaurantId}, ${values})`, (err) => {
                 if (err) {
                   cb(err);
-                } else {
-                  cb(null, restaurantId);
+                } else if (i === features.length - 1) {
+                  cb(null);
                 }
               });
             });
@@ -88,13 +87,30 @@ const removeRestaurant = (id, cb) => {
     } else {
       cb(null, data);
     }
-  })
-}
+  });
+};
+
+const updateRestaurant = ({restaurant, articles, features}, id, cb) => {
+  var columns = Object.keys(restaurant);
+  var updateRestaurant = '';
+  columns.forEach((column) => {
+    updateRestaurant += `${column} = "${restaurant[column]}",`;
+  });
+  updateRestaurant = updateRestaurant.slice(0, -1);
+  connection.query(`UPDATE restaurants SET ${updateRestaurant} WHERE id = ${id};`, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(null);
+    }
+  });
+};
 
 module.exports = {
   getRestaurantInfo,
   getRestaurantArticles,
   getRestaurantFeatures,
   addRestaurant,
-  removeRestaurant
+  removeRestaurant,
+  updateRestaurant
 };
--- server/index.js
+++ server/index.js
@@ -58,8 +58,14 @@ app.post('/api/restaurant', (req, res) => {
 app.delete('/api/restaurant/:id', (req, res) => {
   db.removeRestaurant(req.params.id, (data) => {
     res.sendStatus(err ? 500 : 200);
-  })
-})
+  });
+});
+
+app.put('/api/restaurant/:id', (req, res) => {
+  db.updateRestaurant(req.body, req.params.id, (err) => {
+    res.sendStatus(err ? 500 : 200);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
