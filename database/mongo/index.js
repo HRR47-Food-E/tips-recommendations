d7fa999aee60b0b52b0a99f0a8a7bb1849156c22
Joseph Dry <joe.dry6477@gmail.com>
2020-08-29 14:16:57 -0400

    Changed mongoose schema

--- database/mongo/index.js
+++ database/mongo/index.js
@@ -5,14 +5,10 @@ mongoose.connect('mongodb://localhost/zagattips');
 db.on('connected', () => console.log('Mongoose is connected!'));
 
 let restaurantSchema = new mongoose.Schema({
-  restaurant_name: String,
-  dish_name1: String,
-  dish_name2: String,
-  dish_name3: String,
-  dish_image1: Number,
-  dish_image2: Number,
-  dish_image3: Number,
-  tip: String
+  id: Number,
+  restaurants: Object,
+  articles: Array,
+  features: Array
 });
 
 let Restaurant = mongoose.model('Restaurant', restaurantSchema);
--- database/postgres/seed.js
+++ database/postgres/seed.js
@@ -1,15 +1,6 @@
 const client = require('./config.js');
 const fs = require('fs');
 
-// fs.readFile(__dirname + '/../data.csv', 'utf8', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     data = JSON.parse(data);
-//     console.log(data);
-//   }
-// })
-
 const addRestaurants = `COPY restaurants(restaurant_name,dish_name1,dish_name2,dish_name3,dish_image1,dish_image2,dish_image3,tip) FROM '${__dirname + '/../restaurant-data.csv'}' DELIMITER ',' CSV HEADER;`;
 
 const addArticles = `COPY articles(restaurant_id,title,image) FROM '${__dirname + '/../article-data.csv'}' DELIMITER ',' CSV HEADER;`;
