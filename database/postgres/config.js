ebfa3765c6625c523ce8f09d761ecf06a0f5ac19
Joseph Dry <joe.dry6477@gmail.com>
2020-09-08 18:17:57 -0400

    configure deployed database

--- database/postgres/config.js
+++ database/postgres/config.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const connectionString = `postgres://postgres:password@ec2-3-23-126-178.us-east-2.compute.amazonaws.com:5432/zagattips`
+const connectionString = `postgres://postgres:password@ec2-18-221-11-200.us-east-2.compute.amazonaws.com:5432/zagattips`
 
 const client = new Pool({ connectionString });
 
